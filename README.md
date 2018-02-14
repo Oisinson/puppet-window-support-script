b5c25436835da9ad0d01ca2f2452a29467121153
Oisinson <Oisinson@outlook.com>
2018-02-14 00:53:54 -0800

    Created README summary and added first draft of Windows Support Script

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # puppet-window-support-script
+PowerShell commands meant to gather troubleshooting information for the Puppet Enterprise Windows Agent.
+
+Requires PowerShell version 3 or greater. 
---
+++ window_support.ps1
@@ -0,0 +1,47 @@
+#Setting Variables
+$debugrun = 'puppet agent -t --debug --trace'
+$facterdebug = 'facter --debug --trace'
+$logdir = puppet config print logdir
+$server = puppet config print server
+
+
+# Creating Folder
+$supportfolder = $logdir + "/" + (get-date -f yyyy-MM-dd-hh-mm)
+New-Item -Path $folder  -ItemType directory
+
+
+# Print Screen. Folder Location, Progress
+Write-host = "Support logs are being written to" + $supportfolder
+
+# Getting  system information
+$hostname = 'Hostname' *>> ($supportfolder + '/systeminfo.log')
+hostname *>> ($supportfolder + '/systeminfo.log')
+
+$puppet_version = 'puppet --version' *>> ($supportfolder + '/systeminfo.log')
+puppet --version *>> ($supportfolder + '/systeminfo.log')
+$hiera_version = 'hiera --version' *>> ($supportfolder + '/systeminfo.log')
+Invoke-Expression 'hiera --version' *>> ($supportfolder + '/systeminfo.log')
+$facter_version = 'facter --version' *>> ($supportfolder + '/systeminfo.log')
+Invoke-Expression 'facter --version' *>> ($supportfolder + '/systeminfo.log')
+$ruby_version = 'ruby --verison' *>> ($supportfolder + '/systeminfo.log')
+Invoke-Expression 'ruby -v' *>> ($supportfolder + '/systeminfo.log')
+$where_puppet = 'where puppet' *>> ($supportfolder + '/systeminfo.log')
+where.exe puppet *>> ($supportfolder + '/systeminfo.log')
+$where_ruby = 'where.exe ruby' *>> ($supportfolder + '/systeminfo.log')
+where.exe ruby *>> ($supportfolder + '/systeminfo.log')
+$env_path = 'Environment path' *>> ($supportfolder + '/systeminfo.log')
+$env:path *>> ($supportfolder + '/systeminfo.log')
+
+
+
+$test_connection = 'Testing Connection to Master' *>> ($supportfolder + '/systeminfo.log')
+Test-NetConnection $server -Port 8140 *>> ($supportfolder + '/systeminfo.log')
+$service_info = 'Getting Puppet, PxP-agent, mcollective service information' *>> ($supportfolder + '/systeminfo.log')
+Get-wmiobject -Query "SELECT * FROM win32_service where (name = 'puppet' or name = 'pxp-agent' or name = 'mcollective')" | Format-List -Property Name,Pathname, ProcessId, Startmode, State, Status, Startname *>> ($supportfolder + '/systeminfo.log')
+
+# Gathering existing logs
+Get-Eventlog -source "Puppet" -logname "Application" | Format-List Index, Time, EntryType, Message *>> ($supportfolder + '/pastrunlogs.txt')
+
+# Running Puppet and Facter in debug/trace mode
+Invoke-Expression $debugrun *>> ($supportfolder + '/debugrun.log')
+Invoke-Expression $facterdebug *>> ($supportfolder + '/facterdebug.log')
